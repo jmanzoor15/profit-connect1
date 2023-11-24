94bebf62f33d45923b68ae22bcc3af3ef4bdc80d
Ali Hassan <alihassankp27@gmail.com>
2023-11-24 22:51:58 +0400

    fix: slow hot reload issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,26 @@
 export default defineNuxtConfig({
   css: [
+    "assets/scss/global.scss",
     "assets/scss/formkit.scss",
     "assets/scss/app.scss",
   ],
 
-  plugins: [
-    { src: '~/plugins/bootstrap.client', mode: 'client' },
-  ],
-  modules: [
-    '@pinia/nuxt',
-    '@nuxt/image',
-    '@formkit/nuxt',
-    '@vueuse/nuxt',
-  ],
+  plugins: [{ src: "~/plugins/bootstrap.client", mode: "client" }],
+  modules: ["@pinia/nuxt", "@nuxt/image", "@formkit/nuxt", "@vueuse/nuxt"],
   formkit: {
-    autoImport: true
+    autoImport: true,
   },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/scss/global.scss";',
+          additionalData: '@import "@/assets/scss/_tools/_variables.scss";',
         },
       },
     },
   },
   image: {
-    dir: 'assets/images'
+    dir: "assets/images",
   },
   runtimeConfig: {
     // private variable
@@ -35,5 +29,5 @@ export default defineNuxtConfig({
     public: {
       // for any variable which need to access in both ssr and client side
     },
-},
-})
+  },
+});
