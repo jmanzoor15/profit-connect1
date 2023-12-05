a36cfd2c06f8616b8b304a23907a83d065ae02ab
JUNAID <jmanzoor15@gmail.com>
2023-12-05 16:06:50 +0400

    added multiselect for login

--- server/api/class/add.ts
+++ server/api/class/add.ts
@@ -3,6 +3,7 @@ import useApiFetch from "~/composables/useApi";
 export default defineEventHandler(async ($event) => {
   const api = useApiFetch($event);
   const body = await readBody($event);
+  console.log(body)
   const resp = await api("/category/add/class", {
     method: "POST",
     body,
--- server/api/space/add-category.post.ts
+++ server/api/space/add-category.post.ts
@@ -3,7 +3,8 @@ import useApiFetch from "~/composables/useApi";
 export default defineEventHandler(async ($event) => {
   const body = await readBody($event);
   const api = useApiFetch($event);
-  const resp = await api("/category/add/category", {
+  console.log(body)
+  const resp = await api("/space/add/category", {
     method: "POST",
     body,
   });
