caa8bfd4d7b100441cb9f404a0e401b0ebe9432d
jmanzoor15 <jmanzoor15@gmail.com>
2024-01-02 21:04:21 +0400

    optimized

--- server/api/franchise/current-facility.ts
+++ server/api/franchise/current-facility.ts
@@ -3,7 +3,6 @@ import { cleanObjectL1 } from "@/utils/dataCleaner";
 
 export default defineEventHandler(async ($event) => {
   const body = await readBody($event);
-  console.log(body)
   const api = useApiFetch($event);
   const resp = await api("/franchise/get/franchise", {
     method: "POST",
